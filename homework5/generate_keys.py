b17db2db39f95582971ff8f394a252b0f8495b30
W. Clay Moody <wcmoody@users.noreply.github.com>
2021-03-21 18:23:14 -0400

    Create generate_keys.py

---
+++ homework5/generate_keys.py
@@ -0,0 +1,14 @@
+from Cryptodome.PublicKey import RSA
+from Cryptodome.Cipher import PKCS1_OAEP
+import binascii
+
+keyPair = RSA.generate(1024)
+
+pubKey = keyPair.publickey()
+print(f"Public key:  (n={hex(pubKey.n)}, e={hex(pubKey.e)})")
+pubKeyPEM = pubKey.exportKey()
+print(pubKeyPEM.decode('ascii'))
+
+print(f"Private key: (n={hex(pubKey.n)}, d={hex(keyPair.d)})")
+privKeyPEM = keyPair.exportKey()
+print(privKeyPEM.decode('ascii'))
